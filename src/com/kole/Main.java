f2bede4f82c4113c5b542f008d314c7eb0ecefb0
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-06-14 18:53:43 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -1,8 +1,19 @@
 package com.kole;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] forwardArray = new int[]{1,2,3};
+        reverse(forwardArray);
+    }
 
+    public static void reverse(int[] array){
+        int[] reversedArray = new int[array.length];
+        for (int i= array.length-1;i>-1;i--){
+            reversedArray[array.length-1-i] = array[i];
+        }
+        System.out.println(Arrays.toString(reversedArray));
     }
 }
